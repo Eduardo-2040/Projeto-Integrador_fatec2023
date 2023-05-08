b9610728830d956f0cfa4454fc86d47a8032c1b4
Eduardo Moraes <eduardo2040225@gmail.com>
2023-05-08 14:15:46 -0300

    Comando para calcular fatorial de um dado número

---
+++ Fatorial.py
@@ -0,0 +1,11 @@
+número = int(input('Digite um número: '))
+
+if número == 0:
+    print(f'{número}! = 1')
+
+resultado = 1
+
+for sequencia in range(número, 0, -1):
+    resultado = resultado * sequencia
+
+print(f'{número}! = {resultado}')
