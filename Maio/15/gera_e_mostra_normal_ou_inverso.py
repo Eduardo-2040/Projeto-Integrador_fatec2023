2dfad1e1f09458ebffbf63589c20e1a48c3e9d55
eduardo <eduardo2040225@gmail.com>
2023-05-15 16:01:17 -0300

    gera número e mosta na ordem normal ou inversa

---
+++ Maio/15/gera_e_mostra_normal_ou_inverso.py
@@ -0,0 +1,17 @@
+from random import *
+
+lista = []
+
+escolha = int(input('Disponibilizar na ordem normal = 1 \nDisponibilizar na ordem inversa = 2 \n-> '))
+
+for i in range(10):
+    lista.append(randint(1, 50))
+
+print(f'Lista normal: {lista}')
+
+if escolha == 1:
+    print(f'Sua lista: {lista}')
+
+elif escolha == 2:
+    lista.reverse()
+    print(f'Sua lista: {lista}')
