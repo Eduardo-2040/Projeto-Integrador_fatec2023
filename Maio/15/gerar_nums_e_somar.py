f19c8e12cf2d1f4d168a8a783176d0732e5aac86
eduardo <eduardo2040225@gmail.com>
2023-05-15 14:20:06 -0300

    programa que gera 10 números aleatórios, os amarzena em uma lista e conta quantos pares e impares tem

--- Maio/15/gerar_nums_e_somar.py
+++ Maio/15/gerar_nums_e_somar.py
@@ -1,8 +1,17 @@
 import random
 
 lista = []
+pares = []
+impares = []
 
 for i in range(10):
-    lista.append(random.randint(0, 50))
+    num = random.randint(1,50)
+    lista.append(num)
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
 
-print(lista)
+print(f'lista: {lista}')
+print(f'Pares: {len(pares)}')
+print(f'Impares: {len(impares)}')
